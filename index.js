6e5a740a987fcdb77a4bfe6a7f909c695ea3a173
Sanchit Kumar <sanchit.kr007@gmail.com>
2025-02-02 23:00:58 +0530

    add blog function

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
 const userRoute = require("./routes/user");
+const blogRouter = require("./routes/blogs")
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+const { Blog } = require("./models/blog");
 
 const app = express();
 const PORT = 8002;
@@ -16,13 +18,19 @@ app.set('views', path.resolve('./views'));
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve('./public')))
 
-app.get('/', (req, res)=>{
+app.get('/', async (req, res)=>{
+    const allBlogs = await Blog.find({})
+    console.log(req.user);
     res.render('home',{
         user: req.user,
+        blogs: allBlogs
     });
+    
 })
 
 app.use('/user', userRoute);
+app.use('/blog', blogRouter);
 
 app.listen(PORT, ()=> console.log(`Server started at PORT:${PORT}`)); 
